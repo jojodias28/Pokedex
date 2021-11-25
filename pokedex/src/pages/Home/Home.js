1b313ef4c5d865670942a6d567df3942aa50a481
Hellen Ferreira <contatohellenmarina@gmail.com>
2021-11-25 14:50:44 -0300

    edit

--- pokedex/src/pages/Home/Home.js
+++ pokedex/src/pages/Home/Home.js
@@ -1,52 +1,106 @@
-import React from 'react'
-import { BASE_URL } from '../../contants/Url';
-import useRequestData from '../../Hooks/UseRequestData';
-import Pagination from '@mui/material/Pagination'
+import React from "react";
+import { BASE_URL } from "../../contants/Url";
+import useRequestData from "../../Hooks/UseRequestData";
+import Pagination from "@mui/material/Pagination";
 import { useHistory } from "react-router-dom";
-import { useState } from 'react';
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [numberPage, setNumberPage] = useState(1);
+  const page = numberPage * 20;
+  const [listPokemonData, setListPokemonData] = useRequestData(
+    `${BASE_URL}/pokemon?limit=20&offset=${page}`
+  );
+  const [pokeUrl, setPokeUrl] = useState([]);
+  const [photoUrl, setPhotoUrl] = useState();
 
-    const [numberPage, setNumberPage] = useState(1)
-    const page = numberPage * 20
-    const [listPokemonData] = useRequestData(`${BASE_URL}/pokemon?limit=20&offset=${page}`)
+  const onChangePage = (event, value) => {
+    setNumberPage(value);
+  };
 
+  const history = useHistory();
 
-    const onChangePage = (event, value) => {
-        setNumberPage(value)
-    }
+  const goToPokedex = () => {
+    history.push("/pokedex");
+  };
 
+  const handleData = (res) => {
+    res.data.results.map((item) => {
+      setPokeUrl(pokeUrl.push(item.url));
+    });
+    // console.log(res.data.next)
+    handlePhotos();
+  };
 
-    const history = useHistory();
+  const handlePhotos = () => {
+    pokeUrl.map((pokemon) => getPhotos(pokemon));
+  };
 
-    const goToPokedex = () => {
-        history.push("/pokedex");
-    };
+  const getPhotos = (url) => {
+    axios.get(url).then((response) =>
+      setPhotoUrl(
+        console.log(response.data.sprites.other.home.front_default)
+        // setPhotoUrl(response.data.sprites.other.home.front_default)
+      )
+    );
+  };
 
+  const getData = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20%22")
+      .then((response) => handleData(response));
+  };
 
-    return (
-        <div>
-            <h1> Home </h1>
-            {listPokemonData?.results.map((poke) => {
-                return (
-                    <div>
-                        <p key={poke.id}> {poke.name} </p>
+  useEffect(() => {
+    getData();
+  }, []);
 
-                        {/* <img src={poke.sprites.front_default} />  */}
+  //   const pokeUrl = listPokemonData?.results.map((pokemons) => pokemons.url);
+  //   const filtroPoke = pokeUrl?.map((p) => {
+  //     return p;
+  //   });
 
-                    </div>
-                )
-            })}
+  //   const getUrls = () => {
+  //     const list = [];
+  //     listPokemonData.map((p) => {
+  //       list.push(p.url);
+  //       setListPokemonData(list);
+  //     });
+  //   };
 
-            <button onClick={goToPokedex}> Ir para Pokedex </button>
+  //   console.log(getUrls);
+
+  //   return (
+  //     <div>
+  //       <h1> Home </h1>
+
+  //       {/* <button onClick={goToPokedex}> Ir para Pokedex </button>
+  //       <div>
+  //         <Pagination count={20} color="primary" onChange={onChangePage} />
+  //       </div> */}
+  //     </div>
+  //   );
+  // };
+
+  return (
+    <div>
+      {listPokemonData &&
+        listPokemonData.results.map((poke) => {
+          return (
             <div>
-                <Pagination count={20} color='primary' onChange={onChangePage} />
+              <p key={poke.id}> {poke.name} </p>
+              <img src={photoUrl} />
             </div>
+          );
+        })}
 
-        </div>
-    )
-}
+      <button onClick={goToPokedex}> Ir para Pokedex </button>
+      <div>
+        <Pagination count={20} color="primary" onChange={onChangePage} />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
