d1a274cd40fe6433f16aa6c2e79ea8966ac194e9
Caio Chamarelli <86692521+Caio-Chamarelli@users.noreply.github.com>
2021-11-29 18:38:39 -0300

    Pokedex versão semipronta

--- pokedex/src/pages/Details/Details.js
+++ pokedex/src/pages/Details/Details.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import useRequestData from '../../Hooks/UseRequestData';
+import  { BASE_URL } from '../../contants/Url'; 
 
 const Details = () => {
 
+    const params = useParams()
+    const pokemon = useRequestData(
+        `${BASE_URL}/pokemon/${params.name}`,
+        {}
+    ); 
+
+
+    const pokeType =
+    pokemon.types &&
+    pokemon.types.map((type) => {
+    return <p key={pokemon.id}>{type.type.name.toUpperCase()}</p>;
+    });
+
+const pokeStats =
+    pokemon.stats &&
+    pokemon.stats.map((stat) => {
+    return <p key={pokemon.id}>{stat.base_stat}</p>;
+    });
+
+const pokeMoves =
+    pokemon.moves &&
+    pokemon.moves.slice(0, 3).map((move) => {
+    return <p key={pokemon.id}>{move.move.name}</p>;
+    });
+
     return (
-        <div> <h1> Detalhes </h1></div>
+        <div> 
+            <h1> Detalhes </h1>
+            <p>{pokemon.id}</p>
+            {pokemon.name}
+            {pokeType}
+            {pokemon.height}
+            {pokemon.weight}
+            
+        
+        </div>
     )
 }
 
